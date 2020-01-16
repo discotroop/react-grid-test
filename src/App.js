07a42a9a3a00faf286eb4351d3a1c12f5327b1f4
John Mark Goodson <john.mark.hat@gmail.com>
2020-01-16 17:34:06 -0500

    moving from shop

--- src/App.js
+++ src/App.js
@@ -5,22 +5,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Board />
     </div>
   );
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+    // this.state = {
+    //   // fun stuff
+    // }    
+  buildDivs() {
+    for (i = 0; i < 64; i++) {
+      
+    }
+  }
+
+  render() {
+    return <div className="board"> hi </div>
+  }
 }
 
 export default App;
